727489c42c61c8d9e75ca6003556e48480713632
crpz1 <8588315+crpz1@users.noreply.github.com>
2025-07-07 22:48:46 +1000

    email: switch to brevo (#9)
    * email: switch to brevo
    
    * actions: manual invocation
    
    * actions: whatever
    
    * email: import *

---
+++ src/email/brevo/index.ts
@@ -0,0 +1,25 @@
+import * as brevoMail from '@getbrevo/brevo';
+import loadConfig from '../../config';
+
+const config = loadConfig();
+const apiInstance = new brevoMail.TransactionalEmailsApi();
+apiInstance.setApiKey(brevoMail.TransactionalEmailsApiApiKeys.apiKey, config.email.brevo.apiKey);
+
+const sendEmail = async (email: string, username: string, jwt: string) => {
+    const newEmail = new brevoMail.SendSmtpEmail()
+    const verificationUrl = `${config.web.uri}/confirm?token=${jwt}`;
+
+    newEmail.to = [ { email } ];
+    newEmail.subject = "MACS Discord Verification";
+    newEmail.textContent = `Hi ${username},\n\nPlease click here to verify your Discord account in the MACS server: ${verificationUrl}\n\nIf you did not request this email, please ignore it.`;
+    newEmail.sender = { email: "noreply@macs.codes" }
+
+    try {
+        await apiInstance.sendTransacEmail(newEmail);
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+export default sendEmail;
